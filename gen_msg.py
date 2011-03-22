d4665463664af99c06db3fc3a142c836d49b48d8
Dustin Bachrach <ahdustin@gmail.com>
2011-03-22 17:15:43 -0500

    Finished decryption.

--- gen_msg.py
+++ gen_msg.py
@@ -9,6 +9,12 @@ from Crypto import Random, Cipher
 from Crypto.Hash import HMAC, SHA, SHA256
 
 
+PADDING = '{'
+BLOCK_SIZE = 16
+pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
+EncodeAES = lambda c, s: base64.b64encode(c.encrypt(pad(s)))
+DecodeAES = lambda c, e: c.decrypt(base64.b64decode(e)).rstrip(PADDING)
+
 def decrypt_msg(hoot, plain_tag):
     
     # Removes the short tag before the cipher text
@@ -18,20 +24,38 @@ def decrypt_msg(hoot, plain_tag):
     
     session_integrity_cipher = hoot[0:64]
     integrity = hoot[64:92]
-    message = hoot[92:]
+    message_cipher = hoot[92:]
     
-    print session_integrity_cipher, integrity, message
+    #print session_integrity_cipher, integrity, message_cipher
     
-    # Base64 Decode the cipher text
-    #msg = base64.b64decode(msg)
+    sha = SHA256.new()
+    sha.update(plain_tag)
+    bit_long_tag = sha.digest()
+    
+    aes = Cipher.AES.new(bit_long_tag[16:32])
+    session_integrity_payload = DecodeAES(aes, session_integrity_cipher)
+    
+    session_key = session_integrity_payload[0:16]
+    integrity_key = session_integrity_payload[16:36]
+     
+    
+    hmac = HMAC.new(integrity_key, digestmod=SHA)
+    hmac.update(message_cipher)
+    integrity_computed = base64.b64encode(hmac.digest())
+    
+    if integrity != integrity_computed:
+        print "FAILURE: Integrity not preserved"
+        return
+    
+    aes = Cipher.AES.new(session_key)
+    message = DecodeAES(aes, message_cipher)
+      
+    # print "payload", session_integrity_payload
+    # print "message", message
+    # print "integrity_computed", integrity_computed
 
 
 def encrypt_msg(message, plain_tag, short_tag_length=2): 
-
-    PADDING = '{'
-    BLOCK_SIZE = 16
-    pad = lambda s: s + (BLOCK_SIZE - len(s) % BLOCK_SIZE) * PADDING
-    EncodeAES = lambda c, s: base64.b64encode(c.encrypt(pad(s)))
 
     session_key = Random.get_random_bytes(16)
     integrity_key = Random.get_random_bytes(20)
@@ -47,6 +71,7 @@ def encrypt_msg(message, plain_tag, short_tag_length=2):
     
     
     session_integrity_payload = (session_key + integrity_key)
+    #print "payload", session_integrity_payload
     
     aes = Cipher.AES.new(bit_long_tag[16:32])
     session_integrity_cipher = EncodeAES(aes, session_integrity_payload)
@@ -62,10 +87,10 @@ def encrypt_msg(message, plain_tag, short_tag_length=2):
     
     hoot = "#" + short_tag + " " + session_integrity_cipher + integrity + message_cipher
     
-    print "Input (", len(message), "):", message
-    print "Output (", len(hoot), "):", hoot
-    
-    return hoot
+    # print "Input (", len(message), "):", message
+    # print "Output (", len(hoot), "):", hoot
+    # 
+    # 
     # print "#shorttag", 1 + len(short_tag)
     # print "space", 1
     # print "Integrity", len(integrity)
@@ -73,7 +98,7 @@ def encrypt_msg(message, plain_tag, short_tag_length=2):
     # print "Message Cipher text", len(message_cipher)
     
     # TODO: What about date/time?
-   
+    return hoot   
 
 def main():
     plain_tag = sys.argv[1]
@@ -82,8 +107,9 @@ def main():
     if len(sys.argv) > 3:
         size = int(sys.argv[3])
     
+    hoot = "#fE Lw2BuD9HXARCFJy+xjmDbev4S+qknmamtBNagDJDpT6LJP1vsnJBrxPvV1PgygT0USP2pEdQ1n0UrYYFTynoWTaVL3g=Gowxmt+yfH4/PVGCsh/uROLXNoMcI4x0G3AOkPdvS2E="
     for i in xrange(size):
-        hoot = encrypt_msg(message, plain_tag)
+        #hoot = encrypt_msg(message, plain_tag)
         decrypt_msg(hoot, plain_tag)
 
 
