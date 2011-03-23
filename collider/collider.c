58405f6c61666ee8113ce71679d3fcce035448cf
Christopher Nunu <ChrisN@metatron.(none)>
2011-03-22 23:17:27 -0500

    converted collider to prefix enabled form

--- collider/collider.c
+++ collider/collider.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 // #include <CommonCrypto/CommonDigest.h>
 #include <omp.h>
 #include <string.h>
@@ -10,8 +11,8 @@
 // Mapping from 6 bit pattern to ASCII character.
 //
 static unsigned char base64EncodeLookup[65] =
-	"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
-	
+"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
 //
 // Fundamental sizes of the binary and base64 encode/decode units in bytes
 //
@@ -41,7 +42,7 @@ static unsigned char base64EncodeLookup[65] =
 void NewBase64Encode(const void *buffer, size_t length, int separateLines, size_t *outputLength , char *outputBuffer)
 {
 	const unsigned char *inputBuffer = (const unsigned char *)buffer;
-
+	
 	size_t i = 0;
 	size_t j = 0;
 	const size_t lineLength = separateLines ? INPUT_LINE_LENGTH : length;
@@ -53,7 +54,7 @@ void NewBase64Encode(const void *buffer, size_t length, int separateLines, size_
 		{
 			lineEnd = length;
 		}
-
+		
 		for (; i + BINARY_UNIT_SIZE - 1 < lineEnd; i += BINARY_UNIT_SIZE)
 		{
 			//
@@ -61,9 +62,9 @@ void NewBase64Encode(const void *buffer, size_t length, int separateLines, size_
 			//
 			outputBuffer[j++] = base64EncodeLookup[(inputBuffer[i] & 0xFC) >> 2];
 			outputBuffer[j++] = base64EncodeLookup[((inputBuffer[i] & 0x03) << 4)
-				| ((inputBuffer[i + 1] & 0xF0) >> 4)];
+												   | ((inputBuffer[i + 1] & 0xF0) >> 4)];
 			outputBuffer[j++] = base64EncodeLookup[((inputBuffer[i + 1] & 0x0F) << 2)
-				| ((inputBuffer[i + 2] & 0xC0) >> 6)];
+												   | ((inputBuffer[i + 2] & 0xC0) >> 6)];
 			outputBuffer[j++] = base64EncodeLookup[inputBuffer[i + 2] & 0x3F];
 		}
 		
@@ -87,7 +88,7 @@ void NewBase64Encode(const void *buffer, size_t length, int separateLines, size_
 		//
 		outputBuffer[j++] = base64EncodeLookup[(inputBuffer[i] & 0xFC) >> 2];
 		outputBuffer[j++] = base64EncodeLookup[((inputBuffer[i] & 0x03) << 4)
-			| ((inputBuffer[i + 1] & 0xF0) >> 4)];
+											   | ((inputBuffer[i + 1] & 0xF0) >> 4)];
 		outputBuffer[j++] = base64EncodeLookup[(inputBuffer[i + 1] & 0x0F) << 2];
 		outputBuffer[j++] =	'=';
 	}
@@ -110,80 +111,185 @@ void NewBase64Encode(const void *buffer, size_t length, int separateLines, size_
 	{
 		*outputLength = j;
 	}
-
+	
 }
 
-static const char alphanum[] =
-    "0123456789"
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    "abcdefghijklmnopqrstuvwxyz";
+/*
+ *	Creates an array of size num_digits padded with zeros and fills in the digits of (num)output_base.
+ *  A pointer to the array is assigned to result_buffer; 
+ */
+void baseConverter(int num, int output_base, int num_digits, int** result_buffer) {
+	
+	int* buffer = malloc(num_digits * sizeof(int));
+	int M = num;
+	int offset = 0;
+	
+	for(int i = 0; i < num_digits; i++)
+		buffer[i] = 0;
+	
+	while (M >= output_base) {
+		
+		int temp = M % output_base;
+		buffer[num_digits - offset - 1] = temp;
+		M = M/output_base;
+		offset++;
+		
+		if(offset >= num_digits) {
+			
+			printf("Entered number that exceeded number of digits allowable for num %d, digits %d", num, num_digits);
+			exit(1);
+		}
+	}
+	
+	buffer[0] = M;
+	
+	*result_buffer = buffer;
+}
+
+/*
+ * Increments the array of digits in base -base- by 1, updating subsequent digits if necessary. Will overflow 
+ * if incremented beyond base ^ num_digits - 1, but this is a desired property
+ */
+void baseIncrementer(int* num, int base, int num_digits) {
+	
+	for (int i = num_digits -1; i >= 0; i--) {
+		
+		if(num[i] = base - 1) {
+			
+			num[i] = 0;
+		}
+		else {
+			
+			num[i]++;
+			break;
+		}
+	}
+}
+
+/*
+ *	Returns an int array of size num_threads containing as close to evenly divided
+ *	chunks of the exploration_space_size. This allows any thread to start executing
+ *	from any position in the exploration space and execute from start to 
+ *  start + iterationChunks[thread_num] - 1 without overlapping other threads. 
+ *
+ */
+int* getIterationChunks(int exploration_space_size, int num_threads) {
+	
+	int exploration_chunks = exploration_space_size	/ num_threads;
+	int exploration_chunks_remainder = exploration_space_size % num_threads;
+	int* iterationChunks = malloc(num_threads * sizeof(int));
+	
+	for( int i = 0; i < num_threads; i++ )
+		iterationChunks[i] = exploration_chunks;
+	
+	int index = 0;
+	while (exploration_chunks_remainder != 0 ) {
+		
+		iterationChunks[index % num_threads] += 1;
+		index++;
+		exploration_chunks_remainder--;
+	}
+	
+	return iterationChunks;
+}
+
+static const char* alphanums = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
 int main(int argc, char *argv[])
 {	
-	char *short_tag = argv[1];
-	char *prefix = argv[2];
-	int exploration_space_lower_bound = atoi(argv[3]);
-	int exploration_space_upper_bound = atoi(argv[4]);
-	int exploration_space_size = exploration_space_upper_bound - exploration_space_lower_bound;
-	int num_tags = atoi(argv[5]);
-	int threads = atoi(argv[6]);
+	unsigned char *short_tag = argv[1];
+	unsigned char *prefix = argv[2];
+	int suffix_size = atoi(argv[3]);
+	int num_tags = atoi(argv[4]);
+	int threads = atoi(argv[5]);
+	
 	omp_set_num_threads(threads);
 	
 	int time_seed = time(NULL);
+	int exploration_space_size = (int)pow(64, suffix_size) - 1;
 	
+	if (exploration_space_size < 0) {
+		
+		printf("Num bytes for suffix is too large. Exiting...");
+		return 0;
+	}
 	
-	#pragma omp parallel for default(shared)
-	for (int thread_num = 0; thread_num < threads; thread_num++)
+	//
+	// Byte accurate calculation of final buffer size
+	//
+	size_t long_tag_encoded_size = ((20 / BINARY_UNIT_SIZE) + ((20 % BINARY_UNIT_SIZE) ? 1 : 0)) * BASE64_UNIT_SIZE;
+	
+	//
+	// Include space for a terminating zero
+	//
+	long_tag_encoded_size += 1;
+	
+	srand(time_seed);
+	int startPos = rand() % exploration_space_size;
+	int* iterationChunks = getIterationChunks(exploration_space_size, threads);
+	
+	int shortTagLen = strlen(short_tag);
+	int prefixLen = strlen(prefix);
+	int alphanumLen = strlen(alphanums);
+	unsigned long long tags_checked = 0;
+	
+	#pragma omp parallel for default(none) \
+	private(short_tag, shortTagLen, prefix, prefixLen, suffix_size, long_tag_encoded_size, startPos, threads, iterationChunks, exploration_space_size, alphanums, alphanumLen) \
+	shared(num_tags) \
+	reduction(+: tags_checked)
+	for (int i = 0; i < threads; i++)
 	{		
-		int seed_num = time_seed + thread_num;
-		
-		printf("seed(%d): %d\n", thread_num, time_seed + thread_num);
-
-		int n = strlen(short_tag);
-		
-		char plain_tag[exploration_space_size + 1];
+		unsigned char plain_tag[prefixLen + suffix_size + 1];
+		strncpy(plain_tag, prefix, prefixLen);
 		
 		unsigned char long_tag[20];
 		
-		
-		//
-		// Byte accurate calculation of final buffer size
-		//
-		size_t long_tag_encoded_size =
-				((20 / BINARY_UNIT_SIZE)
-					+ ((20 % BINARY_UNIT_SIZE) ? 1 : 0))
-						* BASE64_UNIT_SIZE;
-
-		//
-		// Include space for a terminating zero
-		//
-		long_tag_encoded_size += 1;
-		
 		char long_tag_encoded[long_tag_encoded_size];
 		
-		unsigned long long int tags_checked = 0;
+		//compute the start position by advancing the start index by chunks
+		int start = startPos;
+		for (int j = 0; j < threads; j++) {
+			
+			if (j = i) {
+				break;
+			}
+			else {
+				
+				start += iterationChunks[j];
+				start = start % exploration_space_size;
+			}
+		}
 		
-		while(num_tags > 0)
+		int* suffix_rep;
+		
+		//initializes the character representation in base 64
+		baseConverter(start, alphanumLen, suffix_size, &suffix_rep);
+		
+		int index = 0;
+		int numIterations = iterationChunks[i];
+		
+		while(num_tags > 0 && index < numIterations)
 		{
+			
 			// Generate random string
-			int len = rand_r(&seed_num) % exploration_space_size + exploration_space_lower_bound;	
-			for (int a = 0; a < len; a++) 
+			for (int a = 1; a <= suffix_size; a++) 
 			{
-		        plain_tag[a] = alphanum[rand_r(&seed_num) % (sizeof(alphanum) - 1)];
+		        plain_tag[prefixLen + a] = alphanums[suffix_rep[suffix_size - a]];
 		    }
-		    plain_tag[len] = '\0';
-
+			
+		    plain_tag[prefixLen + suffix_size + 1] = '\0';
+			
 			//CC_SHA1(plain_tag, len, long_tag);
-			SHA1(plain_tag, len, long_tag);
-
+			SHA1(plain_tag, prefixLen + suffix_size, long_tag);
+			
 			NewBase64Encode(long_tag, 20, 0, NULL, long_tag_encoded);
 			
 			// Compares the input short tag to the long tag truncated to the length of the short tag
-			if (strncmp(short_tag, long_tag_encoded, n) == 0)
+			if (strncmp(short_tag, long_tag_encoded, shortTagLen) == 0)
 			{
-				unsigned char short_tag_new[n + 1];
-				strncpy(short_tag_new, long_tag_encoded, n);
-				short_tag_new[n] = '\0';
+				unsigned char short_tag_new[shortTagLen + 1];
+				strncpy(short_tag_new, long_tag_encoded, shortTagLen);
+				short_tag_new[shortTagLen] = '\0';
 				
 				printf("Found new tag: %s -> %s\n", plain_tag, short_tag_new);
 				#pragma omp critical
@@ -191,9 +297,12 @@ int main(int argc, char *argv[])
 					num_tags--;
 				}
 			}
+			
+			baseIncrementer(suffix_rep, alphanumLen, suffix_size);
 			tags_checked++;
+			index++;
 		}
-		
-		printf("Tags Checked (%d): %llu\n", thread_num, tags_checked);
 	}
+	
+	printf("Num Tags Checked: %llu\n", tags_checked);
 }
